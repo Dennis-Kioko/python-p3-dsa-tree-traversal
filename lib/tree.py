34f3e7cdd4bdf22bc86ec186bc8e2d79700759d8
denniskioko <kiokodennis6990@gmail.com>
2024-03-16 18:14:24 +0300

    Completed assignment

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,20 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, target_id):
+        return self._dfs_search(self.root, target_id)
+
+    def _dfs_search(self, node, target_id):
+        if node is None:
+            return None
+
+        if node['id'] == target_id:
+            return node
+
+        for child in node['children']:
+            result = self._dfs_search(child, target_id)
+            if result is not None:
+                return result
+        
+        return None
